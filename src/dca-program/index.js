8bf8b817cd75fd71c8cfa12c6a6734dd7705039c
Ashish Sapkota <aacsspkt3@gmail.com>
2022-04-29 17:15:56 +0545

    Re-export classes from instruction data

---
+++ src/dca-program/index.js
@@ -0,0 +1,11 @@
+export {
+    DepositSolData,
+    InitializeData,
+    DepositTokenData,
+    WithdrawTokenData,
+    WithdrawSolData,
+    FundTokenData,
+    FundSolData,
+    SwapFromSolData,
+    SwapToSolData
+} from "./instructionData";

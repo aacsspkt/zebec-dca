ab3d928e779c1702ffc0663e27138a09bfa9de89
Ashish Sapkota <aacsspkt3@gmail.com>
2022-05-01 16:09:50 +0545

    use BigNumber package instead of BigInt

--- src/dca-program/utils/helper.js
+++ src/dca-program/utils/helper.js
@@ -1,4 +1,5 @@
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { BigNumber } from "bignumber.js";
 
 
 export const convertToLamports = (amount) => {
@@ -8,10 +9,10 @@ export const convertToLamports = (amount) => {
     if (amount <= 0) {
         throw new RangeError("amount must be greater than 0");
     }
-    amount = BigInt(amount);
-    const lamports = amount * BigInt(LAMPORTS_PER_SOL);
-    if (lamports > ((2n ** 64n) - 1n)) {
+    const amt = new BigNumber(amount);
+    const lamports = amt.multipliedBy(new BigNumber(LAMPORTS_PER_SOL));
+    if (lamports.isGreaterThan(new BigNumber("18446744073709551615"))) {
         throw new RangeError("lamports of given amount doesn\'t fit in unsigned 64-bit integer.");
     }
-    return lamports
+    return lamports.toString()
 }

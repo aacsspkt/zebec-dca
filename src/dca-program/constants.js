c1181628c282d82c245f94694b7ddf4509ccbe99
Ashish Sapkota <aacsspkt3@gmail.com>
2022-05-01 13:33:57 +0545

    reexported imports

--- src/dca-program/constants.js
+++ src/dca-program/constants.js
@@ -1,20 +1,20 @@
-import { PublicKey, SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
-import {
+import { PublicKey } from "@solana/web3.js";
+
+export {
+    /** The  public key of Token Program. */
     TOKEN_PROGRAM_ID,
-    ASSOCIATED_TOKEN_PROGRAM_ID,
-    TOKEN_PROGRAM_ID,
+
+    /** The  public key Associated Token Program. */
     ASSOCIATED_TOKEN_PROGRAM_ID
 } from "@solana/spl-token";
 
+export {
+    /** The public key of System Variable Rent */
+    SYSVAR_RENT_PUBKEY
+} from "@solana/web3.js"
+
 /** The public key DCA Program. */
-const DCA_PROGRAM_ID = new PublicKey("89U3HCacYnqJYUX33EupQRyKLBAqA9vb6tzAATRp19c7");
+export const DCA_PROGRAM_ID = new PublicKey("89U3HCacYnqJYUX33EupQRyKLBAqA9vb6tzAATRp19c7");
 
-/** The  public key of Token Program. */
-const TOKEN_PROGRAM_ID = TOKEN_PROGRAM_ID;
 
-/** The  public key Associated Token Program. */
-const ASSOCIATED_TOKEN_PROGRAM_ID = ASSOCIATED_TOKEN_PROGRAM_ID;
-
-/** The public key of System Variable Rent */
-const RENT_PUBKEY = SYSVAR_RENT_PUBKEY;
 

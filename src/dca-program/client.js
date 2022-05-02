990aa840e49a61a9ae2a33c9661b6b65a2d5ca2b
Ashish Sapkota <aacsspkt3@gmail.com>
2022-05-02 17:40:42 +0545

    add argument and type check in deposit token and complete initialize part

--- src/dca-program/client.js
+++ src/dca-program/client.js
@@ -1,4 +1,4 @@
-import { clusterApiUrl, Connection, PublicKey, Transaction } from "@solana/web3.js";
+import { clusterApiUrl, Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js";
 import { DcaProgram } from "./instructions";
 
 export const getProvider = async () => {
@@ -10,26 +10,41 @@ export const getProvider = async () => {
     }
 };
 
-
-export async function depositToken({ connection, senderAddress, mintAddress, amountInSol }) {
+/**
+ * 
+ * @returns Object
+ */
+export async function depositToken({ connection, fromAddress, mintAddress, amount }) {
     try {
+        if (!connection && !fromAddress && !mintAddress && !amount) {
+            throw new ReferenceError("Missing arguments");
+        }
+
+        if (!(connection instanceof Connection)) {
+            throw new TypeError("Not a solana connection.");
+        }
+
+        let dcaDataAccount = Keypair.generate();
 
         let txn = new Transaction()
             .add(DcaProgram.depositToken({
-                fromAddress: new PublicKey(senderAddress),
+                fromAddress: new PublicKey(fromAddress),
                 mintAddress: new PublicKey(mintAddress),
-                amountInSol: amountInSol
+                dcaDataAddress: dcaDataAccount.publicKey,
+                amount: amount
             }));
-        txn.feePayer = window.solana.publicKey;
+        txn.feePayer = new PublicKey(fromAddress);
         txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
 
-        const signature = window.solana.signAndSendTransaction(txn);
+        const signedTxn = window.solana.signTransaction(txn.serialize());
+        const signature = connection.sendRawTransaction(signedTxn)
         await connection.confirmTransaction(signature, "confirmed");
 
         return {
             status: "success",
             data: {
-                signature: signature
+                signature: signature,
+                dcaDataAddress: dcaDataAccount.publicKey.toBase58()
             }
         }
     } catch (e) {
@@ -38,8 +53,41 @@ export async function depositToken({ connection, senderAddress, mintAddress, amo
 }
 
 
-export async function initialize(args) {
-    throw new Error("Not Implemented");
+export async function initialize({ connection, fromAddress, dcaDataAddress, startTime, dcaAmount, dcaTime, minimumAmountOut }) {
+    try {
+        if (!connection && !fromAddress && !dcaDataAddress && !startTime && !dcaAmount && dcaTime && !minimumAmountOut) {
+            throw new ReferenceError("Missing arguments");
+        }
+
+        if (!(connection instanceof Connection)) {
+            throw new TypeError("Not a solana connection.");
+        }
+
+        let txn = new Transaction()
+            .add(DcaProgram.initialize({
+                fromAddress: fromAddress,
+                dcaDataAddress: dcaDataAddress,
+                startTime: startTime,
+                dcaAmount: dcaAmount,
+                dcaTime: dcaTime,
+                minimumAmountOut: minimumAmountOut
+            }));
+        txn.feePayer = new PublicKey(fromAddress);
+        txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        const signedTxn = window.solana.signTransaction(txn.serialize());
+        const signature = connection.sendRawTransaction(signedTxn)
+        await connection.confirmTransaction(signature, "confirmed");
+
+        return {
+            status: "success",
+            data: {
+                signature: signature,
+            }
+        }
+    } catch (e) {
+
+    }
 }
 
 

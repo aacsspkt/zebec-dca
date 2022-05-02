cead2093ae8be62370a40c416cb03100553cb623
Ashish Sapkota <aacsspkt3@gmail.com>
2022-05-02 17:36:18 +0545

    complete instruction part

--- src/dca-program/instructions.js
+++ src/dca-program/instructions.js
@@ -1,7 +1,7 @@
-import { Keypair, PublicKey, SystemProgram, TransactionInstruction } from "@solana/web3.js";
+import { PublicKey, SystemProgram, TransactionInstruction } from "@solana/web3.js";
 import { DCA_PROGRAM_ID, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, SYSVAR_RENT_PUBKEY } from "./constants"
-import { DepositTokenData } from "./instructionData";
-import { convertToLamports } from "./utils/helper";
+import { DepositSolData, DepositTokenData, FundSolData, FundTokenData, InitializeData, WithdrawSolData, WithdrawTokenData } from "./instructionData";
+import { convertToLamports } from "../utils/convertToLamports";
 
 
 /** 
@@ -32,19 +32,21 @@ export class DcaProgram {
 
     /** 
      * Create Transaction Instruction that deposit non native token to DCA vault
+     * @return 
+     * Transaction Instruction
      */
-    static async depositToken({ fromAddress, mintAddress, dcaDataAddress, amountInSol }) {
+    static async depositToken({ fromAddress, mintAddress, dcaDataAddress, amount }) {
         try {
-            if (!fromAddress instanceof PublicKey &&
-                !mintAddress instanceof PublicKey &&
-                !dcaDataAddress instanceof PublicKey) {
+            if (!(fromAddress instanceof PublicKey) &&
+                !(mintAddress instanceof PublicKey) &&
+                !(dcaDataAddress instanceof PublicKey)) {
                 throw new TypeError("Not a public key.")
             }
             const [vaultAddress,] = await deriveDcaAddress([fromAddress.toBuffer(), dcaDataAddress.toBuffer()]);
             const [senderAta,] = await deriveAssociatedTokenAddress(fromAddress, mintAddress);
             const [vaultAta,] = await deriveAssociatedTokenAddress(vaultAddress, mintAddress);
 
-            const data = new DepositTokenData({ amount: convertToLamports(amountInSol) }).encode();
+            const data = new DepositTokenData({ amount: convertToLamports(amount) }).encode();
 
             return new TransactionInstruction({
                 keys: [
@@ -66,7 +68,7 @@ export class DcaProgram {
                     {
                         pubkey: mintAddress,
                         isSigner: false,
-                        isWritable: false
+                        isWritable: true
                     },
                     {
                         pubkey: SystemProgram.programId,
@@ -107,19 +109,471 @@ export class DcaProgram {
         }
     }
 
-    static async depositSol() { }
 
-    static async initialize() { }
+    /**
+     * Create Transaction Instruction that deposit native token to DCA vault
+     * @returns
+     * TransactionInstruction 
+     */
+    static async depositSol({ fromAddress, mintAddress, dcaDataAddress, amount }) {
+        try {
+            if (!(fromAddress instanceof PublicKey) &&
+                !(mintAddress instanceof PublicKey) &&
+                !(dcaDataAddress instanceof PublicKey)) {
+                throw new TypeError("Not a public key.")
+            }
 
-    static async withdrawToken() { }
+            const [vaultAddress,] = await deriveDcaAddress([fromAddress.toBuffer(), dcaDataAddress.toBuffer()]);
+            const [senderAta,] = await deriveAssociatedTokenAddress(fromAddress, mintAddress);
+            const [vaultAta,] = await deriveAssociatedTokenAddress(vaultAddress, mintAddress);
 
-    static async withdrawSol() { }
+            const data = new DepositSolData({ amount: convertToLamports(amount) }).encode();
 
-    static async fundToken() { }
+            return new TransactionInstruction({
+                keys: [
+                    {
+                        pubkey: fromAddress,
+                        isSigner: true,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: mintAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: SystemProgram.programId,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: SYSVAR_RENT_PUBKEY,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: senderAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: ASSOCIATED_TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: dcaDataAddress,
+                        isSigner: true,
+                        isWritable: true
+                    },
+                ],
+                programId: DCA_PROGRAM_ID,
+                data: data
+            });
 
-    static async fundSol() { }
+        } catch (e) {
+            throw e;
+        }
 
-    static async swapFromSol() { }
+    }
 
-    static async swapToSol() { }
+    /**
+     * Create Transaction Instruction that initialize DCA
+     * @returns TransactionInstruction
+     */
+    static async initialize({ fromAddress, dcaDataAddress, startTime, dcaAmount, dcaTime, minimumAmountOut }) {
+        try {
+            if (!(fromAddress instanceof PublicKey) &&
+                !(dcaDataAddress instanceof PublicKey)) {
+                throw new TypeError("Not a public key.")
+            }
+
+            if (typeof startTime != "number" &&
+                typeof dcaTime != "number"
+            ) {
+                throw new TypeError("Not a number.")
+            }
+            const [vaultAddress,] = await deriveDcaAddress([fromAddress.toBuffer(), dcaDataAddress.toBuffer()]);
+
+            const data = new InitializeData({
+                startTime: startTime,
+                dcaAmount: convertToLamports(dcaAmount),
+                dcaTime: dcaTime,
+                minimumAmountOut: convertToLamports(minimumAmountOut)
+            }).encode();
+
+            return new TransactionInstruction({
+                keys: [
+                    {
+                        pubkey: fromAddress,
+                        isSigner: true,
+                        isWritable: true,
+                    },
+                    {
+                        pubkey: vaultAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: dcaDataAddress,
+                        isSigner: true,
+                        isWritable: true
+                    }
+                ],
+                programId: DCA_PROGRAM_ID,
+                data: data
+            })
+        } catch (e) {
+
+        }
+    }
+
+    /**
+     * Create Transaction Instruction that withdraws token from DCA vault
+     * @returns TransactionInstruction
+     */
+    static async withdrawToken({ fromAddress, mintAddress, dcaDataAddress, transferAmount }) {
+        try {
+            if (!(fromAddress instanceof PublicKey) &&
+                !(mintAddress instanceof PublicKey) &&
+                !(dcaDataAddress instanceof PublicKey)) {
+                throw new TypeError("Not a public key.")
+            }
+
+            const [vaultAddress,] = await deriveDcaAddress([fromAddress.toBuffer(), dcaDataAddress.toBuffer()]);
+            const [senderAta,] = await deriveAssociatedTokenAddress(fromAddress, mintAddress);
+            const [vaultAta,] = await deriveAssociatedTokenAddress(vaultAddress, mintAddress);
+
+            const data = new WithdrawTokenData({ transferAmount: convertToLamports(transferAmount) }).encode();
+
+            return new TransactionInstruction({
+                keys: [
+                    {
+                        pubkey: fromAddress,
+                        isSigner: true,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: mintAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: SystemProgram.programId,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: SYSVAR_RENT_PUBKEY,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: senderAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: ASSOCIATED_TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: dcaDataAddress,
+                        isSigner: true,
+                        isWritable: true
+                    }
+                ],
+                programId: DCA_PROGRAM_ID,
+                data: data
+            })
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Create Transaction Instruction that withdraws token from DCA vault
+     * @returns TransactionInstruction
+     */
+    static async withdrawSol({ fromAddress, mintAddress, dcaDataAddress, transferAmount }) {
+        try {
+            if (!(fromAddress instanceof PublicKey) &&
+                !(mintAddress instanceof PublicKey) &&
+                !(dcaDataAddress instanceof PublicKey)) {
+                throw new TypeError("Not a public key.")
+            }
+
+            const [vaultAddress,] = await deriveDcaAddress([fromAddress.toBuffer(), dcaDataAddress.toBuffer()]);
+            const [senderAta,] = await deriveAssociatedTokenAddress(fromAddress, mintAddress);
+            const [vaultAta,] = await deriveAssociatedTokenAddress(vaultAddress, mintAddress);
+
+            const data = new WithdrawSolData({ transferAmount: convertToLamports(transferAmount) }).encode();
+
+            return new TransactionInstruction({
+                keys: [
+                    {
+                        pubkey: fromAddress,
+                        isSigner: true,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: mintAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: SystemProgram.programId,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: SYSVAR_RENT_PUBKEY,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: senderAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: ASSOCIATED_TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: dcaDataAddress,
+                        isSigner: true,
+                        isWritable: true
+                    }
+                ],
+                programId: DCA_PROGRAM_ID,
+                data: data
+            })
+        } catch (e) {
+            throw e;
+        }
+    }
+
+
+    /**
+     * Create Transaction Instruction that // todo
+     * @returns TransactionInstruction
+     */
+    static async fundToken({ fromAddress, mintAddress, dcaDataAddress, transferAmount }) {
+        try {
+            if (!(fromAddress instanceof PublicKey) &&
+                !(mintAddress instanceof PublicKey) &&
+                !(dcaDataAddress instanceof PublicKey)) {
+                throw new TypeError("Not a public key.")
+            }
+
+            const [vaultAddress,] = await deriveDcaAddress([fromAddress.toBuffer(), dcaDataAddress.toBuffer()]);
+            const [senderAta,] = await deriveAssociatedTokenAddress(fromAddress, mintAddress);
+            const [vaultAta,] = await deriveAssociatedTokenAddress(vaultAddress, mintAddress);
+
+            const data = new FundTokenData({ transferAmount: convertToLamports(transferAmount) }).encode();
+
+            return new TransactionInstruction({
+                keys: [
+                    {
+                        pubkey: fromAddress,
+                        isSigner: true,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: mintAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: SystemProgram.programId,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: SYSVAR_RENT_PUBKEY,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: senderAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: ASSOCIATED_TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: dcaDataAddress,
+                        isSigner: true,
+                        isWritable: true
+                    }
+                ],
+                programId: DCA_PROGRAM_ID,
+                data: data
+            })
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Create Transaction Instruction that // todo
+     * @returns TransactionInstruction
+     */
+    static async fundSol({ fromAddress, mintAddress, dcaDataAddress, transferAmount }) {
+        try {
+            if (!(fromAddress instanceof PublicKey) &&
+                !(mintAddress instanceof PublicKey) &&
+                !(dcaDataAddress instanceof PublicKey)) {
+                throw new TypeError("Not a public key.")
+            }
+
+            const [vaultAddress,] = await deriveDcaAddress([fromAddress.toBuffer(), dcaDataAddress.toBuffer()]);
+            const [senderAta,] = await deriveAssociatedTokenAddress(fromAddress, mintAddress);
+            const [vaultAta,] = await deriveAssociatedTokenAddress(vaultAddress, mintAddress);
+
+            const data = new FundSolData({ transferAmount: convertToLamports(transferAmount) }).encode();
+
+            return new TransactionInstruction({
+                keys: [
+                    {
+                        pubkey: fromAddress,
+                        isSigner: true,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: mintAddress,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: SystemProgram.programId,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: SYSVAR_RENT_PUBKEY,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: senderAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: vaultAta,
+                        isSigner: false,
+                        isWritable: true
+                    },
+                    {
+                        pubkey: ASSOCIATED_TOKEN_PROGRAM_ID,
+                        isSigner: false,
+                        isWritable: false
+                    },
+                    {
+                        pubkey: dcaDataAddress,
+                        isSigner: true,
+                        isWritable: true
+                    }
+                ],
+                programId: DCA_PROGRAM_ID,
+                data: data
+            })
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Create Transaction Instruction that // todo
+     * @returns TransactionInstruction
+     */
+    static async swapFromSol() {
+        throw new Error("Not Implemented");
+    }
+
+
+    /**
+     * Create Transaction Instruction that // todo
+     * @returns TransactionInstruction
+     */
+    static async swapToSol() {
+        throw new Error("Not Implemented");
+    }
 } 

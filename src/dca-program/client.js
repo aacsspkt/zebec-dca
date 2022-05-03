55ed3c2c75b120f79096025df490f6ebf6241b62
Ashish Sapkota <aacsspkt3@gmail.com>
2022-05-03 14:37:05 +0545

    Complete client part

--- src/dca-program/client.js
+++ src/dca-program/client.js
@@ -12,22 +12,22 @@ export const getProvider = async () => {
 
 /**
  * 
- * @returns Object
+ * @returns Response object
  */
 export async function depositToken({ connection, fromAddress, mintAddress, amount }) {
     try {
         if (!connection && !fromAddress && !mintAddress && !amount) {
-            throw new ReferenceError("Missing arguments");
+            throw new ReferenceError("Missing arguments.");
         }
 
         if (!(connection instanceof Connection)) {
-            throw new TypeError("Not a solana connection.");
+            throw new TypeError("Not a Rpc endpoint.");
         }
 
         let dcaDataAccount = Keypair.generate();
 
         let txn = new Transaction()
-            .add(DcaProgram.depositToken({
+            .add(await DcaProgram.depositToken({
                 fromAddress: new PublicKey(fromAddress),
                 mintAddress: new PublicKey(mintAddress),
                 dcaDataAddress: dcaDataAccount.publicKey,
@@ -37,7 +37,7 @@ export async function depositToken({ connection, fromAddress, mintAddress, amoun
         txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
 
         const signedTxn = window.solana.signTransaction(txn.serialize());
-        const signature = connection.sendRawTransaction(signedTxn)
+        const signature = connection.sendRawTransaction(signedTxn);
         await connection.confirmTransaction(signature, "confirmed");
 
         return {
@@ -56,17 +56,17 @@ export async function depositToken({ connection, fromAddress, mintAddress, amoun
 export async function initialize({ connection, fromAddress, dcaDataAddress, startTime, dcaAmount, dcaTime, minimumAmountOut }) {
     try {
         if (!connection && !fromAddress && !dcaDataAddress && !startTime && !dcaAmount && dcaTime && !minimumAmountOut) {
-            throw new ReferenceError("Missing arguments");
+            throw new ReferenceError("Missing arguments.");
         }
 
         if (!(connection instanceof Connection)) {
-            throw new TypeError("Not a solana connection.");
+            throw new TypeError("Not a Rpc enpoint.");
         }
 
         let txn = new Transaction()
-            .add(DcaProgram.initialize({
-                fromAddress: fromAddress,
-                dcaDataAddress: dcaDataAddress,
+            .add(await DcaProgram.initialize({
+                fromAddress: new PublicKey(fromAddress),
+                dcaDataAddress: new PublicKey(dcaDataAddress),
                 startTime: startTime,
                 dcaAmount: dcaAmount,
                 dcaTime: dcaTime,
@@ -76,7 +76,118 @@ export async function initialize({ connection, fromAddress, dcaDataAddress, star
         txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
 
         const signedTxn = window.solana.signTransaction(txn.serialize());
-        const signature = connection.sendRawTransaction(signedTxn)
+        const signature = connection.sendRawTransaction(signedTxn);
+        await connection.confirmTransaction(signature, "confirmed");
+
+        return {
+            status: "success",
+            data: {
+                signature: signature,
+            }
+        }
+    } catch (e) {
+        throw e;
+    }
+}
+
+
+export async function depositSol(connection, fromAddress, mintAddress, amount) {
+    try {
+        if (!connection && !fromAddress && !mintAddress && !amount) {
+            throw new ReferenceError("Missing arguments.");
+        }
+
+        if (!(connection instanceof Connection)) {
+            throw new TypeError("Not a Rpc enpoint.");
+        }
+
+        let dcaDataAccount = Keypair.generate();
+
+        let txn = new Transaction()
+            .add(await DcaProgram.depositSol({
+                fromAddress: new PublicKey(fromAddress),
+                mintAddress: new PublicKey(mintAddress),
+                dcaDataAddress: dcaDataAccount.publicKey,
+                amount: amount,
+            }));
+        txn.feePayer = new PublicKey(fromAddress);
+        txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        const signedTxn = window.solana.signTransaction(txn.serialize());
+        const signature = connection.sendRawTransaction(signedTxn);
+        await connection.confirmTransaction(signature, "confirmed");
+
+        return {
+            status: "success",
+            data: {
+                signature: signature,
+                dcaDataAddress: dcaDataAccount.publicKey.toBase58()
+            }
+        }
+    } catch (e) {
+        throw e;
+    }
+}
+
+
+export async function withdrawToken({ connection, fromAddress, dcaDataAddress, mintAddress, transferAmount }) {
+    try {
+        if (!connection && !fromAddress && !mintAddress && !dcaDataAddress && !transferAmount) {
+            throw new ReferenceError("Missing arguments.");
+        }
+
+        if (!(connection instanceof Connection)) {
+            throw new TypeError("Not a Rpc enpoint.");
+        }
+
+        let txn = new Transaction()
+            .add(await DcaProgram.withdrawToken({
+                fromAddress: new PublicKey(fromAddress),
+                mintAddress: new PublicKey(mintAddress),
+                dcaDataAddress: new PublicKey(dcaDataAddress),
+                transferAmount: transferAmount,
+            }));
+        txn.feePayer = new PublicKey(fromAddress);
+        txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        const signedTxn = window.solana.signTransaction(txn.serialize());
+        const signature = connection.sendRawTransaction(signedTxn);
+        await connection.confirmTransaction(signature, "confirmed");
+
+        return {
+            status: "success",
+            data: {
+                signature: signature,
+            }
+        }
+    } catch (e) {
+        throw e;
+    }
+}
+
+
+export async function withdrawSol({ connection, fromAddress, mintAddress, dcaDataAddress, transferAmount }) {
+    try {
+        if (!connection && !fromAddress && !mintAddress && !dcaDataAddress && !transferAmount) {
+            throw new ReferenceError("Missing arguments.");
+        }
+
+        if (!(connection instanceof Connection)) {
+            throw new TypeError("Not a Rpc enpoint.");
+        }
+
+        let txn = new Transaction()
+            .add(await DcaProgram.withdrawSol({
+                fromAddress: new PublicKey(fromAddress),
+                mintAddress: new PublicKey(mintAddress),
+                dcaDataAddress: new PublicKey(dcaDataAddress),
+                transferAmount: transferAmount
+            }));
+        txn.feePayer = new PublicKey(fromAddress);
+        txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        const signedTxn = window.solana.signTransaction(txn.serialize());
+        const signature = connection.sendRawTransaction(signedTxn);
         await connection.confirmTransaction(signature, "confirmed");
 
         return {
@@ -91,21 +202,6 @@ export async function initialize({ connection, fromAddress, dcaDataAddress, star
 }
 
 
-export async function depositSol(args) {
-    throw new Error("Not Implemented");
-}
-
-
-export async function withdrawToken(args) {
-    throw new Error("Not Implemented");
-}
-
-
-export async function withdrawSol(args) {
-    throw new Error("Not Implemented");
-}
-
-
 export async function swapFromSol(args) {
     throw new Error("Not Implemented");
 }
@@ -116,11 +212,73 @@ export async function swapToSol(args) {
 }
 
 
-export async function fundToken(args) {
-    throw new Error("Not Implemented");
+export async function fundToken({ connection, fromAddress, mintAddress, dcaDataAddress, transferAmount }) {
+    try {
+        if (!connection && !fromAddress && !mintAddress && !dcaDataAddress && !transferAmount) {
+            throw new ReferenceError("Missing arguments.");
+        }
+
+        if (!(connection instanceof Connection)) {
+            throw new TypeError("Not a Rpc endpoint.");
+        }
+
+        let txn = new Transaction()
+            .add(await DcaProgram.fundToken({
+                fromAddress: new PublicKey(fromAddress),
+                mintAddress: new PublicKey(mintAddress),
+                dcaDataAddress: new PublicKey(dcaDataAddress),
+                transferAmount: transferAmount
+            }));
+        txn.feePayer = new PublicKey(fromAddress);
+        txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        const signedTxn = window.solana.signTransaction(txn.serialize());
+        const signature = connection.sendRawTransaction(signedTxn);
+        await connection.confirmTransaction(signature, "confirmed");
+
+        return {
+            status: "success",
+            data: {
+                signature: signature,
+            }
+        }
+    } catch (e) {
+        throw e;
+    }
 }
 
 
-export async function fundSol(args) {
-    throw new Error("Not Implemented");
+export async function fundSol({ connection, fromAddress, mintAddress, dcaDataAddress, transferAmount }) {
+    try {
+        if (!connection && !fromAddress && !mintAddress && !dcaDataAddress && !transferAmount) {
+            throw new ReferenceError("Missing arguments.");
+        }
+
+        if (!(connection instanceof Connection)) {
+            throw new TypeError("Not a Rpc endpoint.");
+        }
+
+        let txn = new Transaction()
+            .add(await DcaProgram.fundSol({
+                fromAddress: new PublicKey(fromAddress),
+                mintAddress: new PublicKey(mintAddress),
+                dcaDataAddress: new PublicKey(dcaDataAddress),
+                transferAmount: transferAmount
+            }));
+        txn.feePayer = new PublicKey(fromAddress);
+        txn.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        const signedTxn = window.solana.signTransaction(txn.serialize());
+        const signature = connection.sendRawTransaction(signedTxn);
+        await connection.confirmTransaction(signature, "confirmed");
+
+        return {
+            status: "success",
+            data: {
+                signature: signature,
+            }
+        }
+    } catch (e) {
+        throw e;
+    }
 }

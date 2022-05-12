bd624ea036ffb3de27f4635671264e7195072e06
Ashish Sapkota <aacsspkt3@gmail.com>
2022-05-13 00:08:04 +0545

    Add raydium and serum program id in constant

--- src/dca-program/constants.js
+++ src/dca-program/constants.js
@@ -1,21 +1,26 @@
 import { PublicKey, Connection, clusterApiUrl } from "@solana/web3.js";
 
 export {
-    /** The  public key of Token Program. */
+    /** The  id of Token Program. */
     TOKEN_PROGRAM_ID,
 
-    /** The  public key of Associated Token Program. */
+    /** The id of Associated Token Program. */
     ASSOCIATED_TOKEN_PROGRAM_ID
 } from "@solana/spl-token";
 
 export {
-    /** The public key of System Variable Rent */
+    /** The id of System Variable Rent */
     SYSVAR_RENT_PUBKEY
 } from "@solana/web3.js"
 
-/** The public key DCA Program. */
+/** The id of DCA Program. */
 export const DCA_PROGRAM_ID = new PublicKey("89U3HCacYnqJYUX33EupQRyKLBAqA9vb6tzAATRp19c7");
 
 /** Json RPC client to communicate with Solana blockchain */
 export const connection = new Connection(clusterApiUrl("devnet"));
 
+/** The id of raydium program liquidity pool amm */
+export const RAYDIUM_PROGRAM_ID = new PublicKey("9rpQHSyFVM1dkkHFQ2TtTzPEW7DVmEyPmN8wVniqJtuC"); // liquidity pool amm
+
+
+export const SERUM_PROGRAM_ID = new PublicKey("");

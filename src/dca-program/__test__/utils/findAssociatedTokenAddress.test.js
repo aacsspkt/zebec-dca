70ec62b11422846ef3b2e0d4d1dc5547053cb2ac
Ashish Sapkota <aacsspkt3@gmail.com>
2022-05-23 16:25:51 +0545

    Add test script for findAssociatedTokenAddress

--- src/dca-program/__test__/utils/findAssociatedTokenAddress.test.js
+++ src/dca-program/__test__/utils/findAssociatedTokenAddress.test.js
@@ -1,4 +1,10 @@
+import { PublicKey } from "@solana/web3.js";
+import { findAssociatedTokenAddress } from "../../utils";
 
-test("find associated token address test", () => {
-    // todo
+test("find associated token address test", async () => {
+    const pubkey1 = new PublicKey("B4mrP9hMMLBWKrB467wWUmHNYStZkuDXxiXYPn5emsuM");
+    const pubkey2 = new PublicKey("GCMt7FsZq5EkkdiCmVk97ZHSTgbrvZNnZSjzarpV5faH");
+    const expected = new PublicKey("C51gTJU2fNz8VzAw9Xpv5ZtFoCSPuiExhWgwn5vEpNkm");
+    const [actual] = await findAssociatedTokenAddress(pubkey1, pubkey2);
+    expect(actual).toEqual(expected);
 });
